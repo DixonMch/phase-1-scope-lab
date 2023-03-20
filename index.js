bf19b3296325b02a3557c8e984e596c302f392ae
DixonMch <dixonmogaka@gmail.com>
2023-03-20 16:52:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = "bob"
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+    // Declare a global variable called bestCustomer using the var keyword
+    var bestCustomer = "not bob";
+    
+ }
+
+ function setBestCustomer(){
+    bestCustomer = "not bob"
+ }
+ function overwriteBestCustomer(){
+    bestCustomer = "maybe bob"
+ }
+const leastFavoriteCustomer= "Jane"
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "John"
+}
+  
